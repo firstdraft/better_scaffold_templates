6cc8d379551b6c23645476199888797226b91362
Raghu Betina <raghu@firstdraft.com>
2017-11-01 11:55:04 -0500

    Update generators.rb

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,5 +1,9 @@
 Rails.application.config.generators do |g|
-  g.scaffold_stylesheet false
-  g.template_engine :all
+  g.assets false
   g.fallbacks[:all] = :erb
+  g.helper false
+  g.javascripts false
+  g.scaffold_stylesheet false
+  g.stylesheets false
+  g.template_engine :all
 end
